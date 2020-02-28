8ab17d7d235c7895e7ce5fef3bb3f4356a072f74
adrien <adrien.s@laposte.net>
2020-02-28 16:14:15 +0100

    first commit

---
+++ todos.php
@@ -0,0 +1,8 @@
+
+[
+
+    {"name": "Enregistrer un tuto angular", "completed": false},
+    {"name": "Monter le tuto sur angular", "completed": false},
+    {"name": "Montrer comment l'ajax c'est trop facile", "completed":true}
+    
+]
